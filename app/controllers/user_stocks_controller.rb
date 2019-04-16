ef3707bdecb1131b67ce3b780409a035ecba3672
marcosg <marcosg101@gmail.com>
2019-04-16 08:47:31 -0400

    Update views and navigation

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -14,7 +14,7 @@ class UserStocksController < ApplicationController
     stock = Stock.find(params[:id])
     @user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
     @user_stock.destroy
-    flash[:notice] = "Stock was successfully removed from portfolio"
+    flash[:info] = "Stock was successfully removed from portfolio"
     redirect_to my_portfolio_path
   end
 end
